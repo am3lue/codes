c0e054628043f44e14137f2aa85188d8134a6f80
Blue Francis <am3lue@gmail.com>
2026-01-20 14:44:17 +0530

    `Updated data.txt with new dates and values`

---
+++ cpp/first_program/simple_calc.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Please Feel At Home ! \n";
+    int a , b;
+
+    cout << "Please Enter"
+}
---
+++ cpp/into.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+// this is CAP 281 Object oriented programinfg using CPP
+int main(){
+    int a = 10;
+    int b = 20;
+    cout<<"The value of a is "<<a<<endl;
+    cout<<"The value of b is "<<b<<endl;
+    cout<<"The sum of a and b is "<<a+b<<endl;
+    return 0;
+}
+
+/*
+wha we will learn in this course
+1. Basic syntax of C++
+2. Object oriented programming concepts
+3. Classes and objects
+4. Inheritance
+5. Polymorphism
+6. Encapsulation
+7. Abstraction
+8. Constructors and destructors
+9. Operator overloading
+10. Templates
+11. Exception handling
+12. Standard Template Library (STL)
+13. File handling
+14. Advanced topics like smart pointers, lambda expressions, etc.
+
+*/
